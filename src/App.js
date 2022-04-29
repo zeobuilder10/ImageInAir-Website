75913b7af937ab6a0561ae2ae17fd98da9c2afc9
zeobuilder10 <adrienclemenceau1@gmail.com>
2022-04-29 10:58:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import { FaInfoCircle } from "react-icons/fa";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="Div-header">
+          <div className="Gho-header"></div>
+          <p className="Title">Image In Air</p>
+
+
+          <Popup trigger={<button className="But-header"><FaInfoCircle className='Ico-header' size={50} /></button>} modal nested>
+            {close => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Informations </div>
+                <div className="content">
+                  I weep as so called gods burn my home asunder, One should never meet such fate and yet here I remain.
+                  I hoped to see them smite me, make the suffering end, but alas their immense minds wouldn't show me such
+                  pity. I wish someday to see my family once more, but I don't think the gods will let me, if they have not 
+                  been cast aside already.
+                </div>
+              </div>
+            )}
+          </Popup>
+
+
+        </div>
       </header>
     </div>
   );
